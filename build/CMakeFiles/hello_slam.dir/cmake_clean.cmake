34d4072925abc6d7c0b2dafe133d6725e76463d7
big-bao <13580467643@163.com>
2016-12-24 12:14:10 +0800

    code directory structure

---
+++ build/CMakeFiles/hello_slam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hello_slam.dir/src/hello_slam.cpp.o"
+  "../bin/hello_slam.pdb"
+  "../bin/hello_slam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hello_slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
